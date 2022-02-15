eff78f2047a8693208b623ee9bc3b3aac94dede5
Tomasz <stonka123@gmail.com>
2022-02-15 18:46:44 +0100

    header plz

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
 burgerBtn = document.querySelector('.hamburger')
 navItems = document.querySelector('.nav__items')
+navBarLogo = document.querySelector('.nav__top-logo')
 
 const showNav = () => {
 	burgerBtn.classList.toggle('is-active')
 	navItems.classList.toggle('nav__items--active')
+	checkNav()
 }
+
+const checkNav = () => {
+	if (navItems.classList.contains('nav__items--active')) {
+		navBarLogo.style.color = 'black'
+	} else {
+		navBarLogo.style.color = 'rgb(233, 230, 225)'
+	}
+}
+
 burgerBtn.addEventListener('click', showNav)
